46e199b7940596713e28e8af5119bd16df0c1117
Lucien-Hsu <meteor831@gmail.com>
2020-12-03 22:47:08 +0800

    feat:完成購物車頁面下方 Layout 與導航 1.製作好購物車部分的 navigation 2.製作好購物車頁面的下方小計與按鈕 3.製作了暫代的 navigationBar 供視覺呈現
    #issue 001

---
+++ app/src/main/java/com/example/buyhome_lcn/fragment_cart/ShoppingCart.java
@@ -0,0 +1,34 @@
+package com.example.buyhome_lcn.fragment_cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.buyhome_lcn.R;
+
+public class ShoppingCart extends Fragment {
+    Button btnGoCheckDeal;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
+
+        btnGoCheckDeal = view.findViewById(R.id.btn_go_checkdeal);
+        btnGoCheckDeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_shoppingCart_to_checkDeal);
+            }
+        });
+
+
+        return view;
+    }
+}
