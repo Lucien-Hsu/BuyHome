fd316b9b26277cd37bf46c7d01f429e279476431
Lucien-Hsu <meteor831@gmail.com>
2020-12-06 00:17:44 +0800

    feat:完成付款頁面 layout 與按鈕
    #issue 004

--- app/src/main/java/com/example/buyhome_lcn/fragment_cart/DoPay.java
+++ app/src/main/java/com/example/buyhome_lcn/fragment_cart/DoPay.java
@@ -1,66 +1,66 @@
 package com.example.buyhome_lcn.fragment_cart;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.buyhome_lcn.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DoPay#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DoPay extends Fragment {
+    Context context;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DoPay() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DoPay.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DoPay newInstance(String param1, String param2) {
-        DoPay fragment = new DoPay();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button btnPay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_do_pay, container, false);
+        context = getActivity();
+
+        View view = inflater.inflate(R.layout.fragment_do_pay, container, false);
+
+        btnPay = view.findViewById(R.id.btn_pay);
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Google Pay 付款
+                doPay();
+            }
+        });
+
+        return view;
+    }
+
+    /**
+     * 付款處理
+     */
+    private void doPay() {
+        //TODO 處理付款
+
+        //[彈出視窗]  付款成功後彈出此視窗
+        View dialogView = getLayoutInflater().inflate(R.layout.alertdialog_dopay, null);
+        final AlertDialog dialog = new AlertDialog
+                .Builder(context)
+                .setView(dialogView).show();
+
+        //[按鈕]  關閉 Activity
+        Button btnOK = dialog.findViewById(R.id.btn_ok);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+                dialog.dismiss();
+            }
+        });
+
     }
 }
