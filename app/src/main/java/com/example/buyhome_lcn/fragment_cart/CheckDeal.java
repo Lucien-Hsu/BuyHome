bbda9e768216715407460f04008175f624693bec
Lucien-Hsu <meteor831@gmail.com>
2020-12-05 21:10:55 +0800

    feat:製作確認付款頁面之折價券 layout / 修改id名稱 / 增加按鈕功能 1.製作確認付款頁面之折價券 layout 2.修改了購物車頁面 layout 的元件 id 3.修改了確認付款頁面 layout 的元件 id 2.新增了確認付款頁面之按鈕功能
    #issue 003

--- app/src/main/java/com/example/buyhome_lcn/fragment_cart/CheckDeal.java
+++ app/src/main/java/com/example/buyhome_lcn/fragment_cart/CheckDeal.java
@@ -3,64 +3,32 @@ package com.example.buyhome_lcn.fragment_cart;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.buyhome_lcn.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CheckDeal#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CheckDeal extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CheckDeal() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CheckDeal.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CheckDeal newInstance(String param1, String param2) {
-        CheckDeal fragment = new CheckDeal();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button btnGoDoPay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_deal, container, false);
+        View view = inflater.inflate(R.layout.fragment_check_deal, container, false);
+
+        btnGoDoPay = view.findViewById(R.id.btn_go_dopay);
+        btnGoDoPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_checkDeal_to_doPay);
+            }
+        });
+
+
+        return view;
     }
 }
