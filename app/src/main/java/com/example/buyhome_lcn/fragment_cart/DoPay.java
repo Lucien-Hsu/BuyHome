70ef38535caf76d45901fcd049aa64320c7a4166
Lucien-Hsu <meteor831@gmail.com>
2020-12-14 20:58:14 +0800

    feat:持續整合 Google Pay 顯示按鈕。
    #issue 040

--- app/src/main/java/com/example/buyhome_lcn/fragment_cart/DoPay.java
+++ app/src/main/java/com/example/buyhome_lcn/fragment_cart/DoPay.java
@@ -17,13 +17,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.buyhome_lcn.R;
 import com.example.buyhome_lcn.util.PaymentsUtil;
 import com.google.android.gms.common.internal.Constants;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
 import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentDataRequest;
 import com.google.android.gms.wallet.PaymentsClient;
 import com.google.android.gms.wallet.Wallet;
 
@@ -32,6 +35,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Optional;
+import java.util.concurrent.Executor;
 
 
 public class DoPay extends Fragment {
@@ -40,7 +44,14 @@ public class DoPay extends Fragment {
     //1-1.用戶端，用於與 Google Pay API 互動
     private PaymentsClient paymentsClient;
 
-    Button btnPay;
+    //8-1.宣告按鈕
+    View googlePayButton;
+
+    //8-6.交易序號，用於追蹤該筆交易
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 991;
+    //8-7.運費
+    private static final long SHIPPING_COST_CENTS = 90 * PaymentsUtil.CENTS_IN_A_UNIT.longValue();
+//    Button btnPay;
 
 //    // Google Pay
 //    private PaymentsClient paymentsClient;
@@ -115,6 +126,7 @@ public class DoPay extends Fragment {
 //        }
 //    }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -136,9 +148,107 @@ public class DoPay extends Fragment {
 //            }
 //        });
 
+        //8-2.[按鈕] 取得按鈕
+        googlePayButton = view.findViewById(R.id.googlePayButton);
+        googlePayButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        requestPayment(view);
+                    }
+                });
+
+        //8-3.確認使用者能夠使用 Google Pay 並顯示按鈕
+        possiblyShowGooglePayButton();
+
         return view;
     }
 
+    //8-4.確認使用者能夠使用 Google Pay ，若可以則顯示按鈕
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void possiblyShowGooglePayButton() {
+
+        final Optional<JSONObject> isReadyToPayJson = PaymentsUtil.getIsReadyToPayRequest();
+        if (!isReadyToPayJson.isPresent()) {
+            return;
+        }
+
+        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
+        // OnCompleteListener to be triggered when the result of the call is known.
+        //確認是否可付款
+        IsReadyToPayRequest request = IsReadyToPayRequest.fromJson(isReadyToPayJson.get().toString());
+        //若可付款，則顯示付款按鈕
+        Task<Boolean> task = paymentsClient.isReadyToPay(request);
+        task.addOnCompleteListener((Activity)context,
+                new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            setGooglePayAvailable(task.getResult());
+                        } else {
+                            Log.w("isReadyToPay failed", task.getException());
+                        }
+                    }
+                });
+    }
+    /**
+     * If isReadyToPay returned {@code true}, show the button and hide the "checking" text. Otherwise,
+     * notify the user that Google Pay is not available. Please adjust to fit in with your current
+     * user flow. You are not required to explicitly let the user know if isReadyToPay returns {@code
+     * false}.
+     *
+     * @param available isReadyToPay API response.
+     */
+    private void setGooglePayAvailable(boolean available) {
+        if (available) {
+            googlePayButton.setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(context, R.string.googlepay_status_unavailable, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //8-5.顯示 Google Pay 頁面
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void requestPayment(View view) {
+
+        // Disables the button to prevent multiple clicks.
+        googlePayButton.setClickable(false);
+
+        // The price provided to the API should include taxes and shipping.
+        // This price is not displayed to the user.
+        //價格會包含稅率與運費，僅用於結帳，不會顯示給使用者知道。
+//        try {
+
+        //[設定價格]
+        //範例是取得當前顯示的商品再取得價格
+        //這裡為了方便，直接給予價格。
+        double garmentPrice = 500;
+//            double garmentPrice = selectedGarment.getDouble("price");
+
+        long garmentPriceCents = Math.round(garmentPrice * PaymentsUtil.CENTS_IN_A_UNIT.longValue());
+        long priceCents = garmentPriceCents + SHIPPING_COST_CENTS;
+
+        Optional<JSONObject> paymentDataRequestJson = PaymentsUtil.getPaymentDataRequest(priceCents);
+        if (!paymentDataRequestJson.isPresent()) {
+            return;
+        }
+
+        PaymentDataRequest request =
+                PaymentDataRequest.fromJson(paymentDataRequestJson.get().toString());
+
+        // Since loadPaymentData may show the UI asking the user to select a payment method, we use
+        // AutoResolveHelper to wait for the user interacting with it. Once completed,
+        // onActivityResult will be called with the result.
+        // 這裡真正顯示結帳頁面
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                    paymentsClient.loadPaymentData(request),
+                    (Activity) context, LOAD_PAYMENT_DATA_REQUEST_CODE);
+        }
+//        } catch (JSONException e) {
+//            throw new RuntimeException("The price cannot be deserialized from the JSON object.");
+//        }
+    }
 
 //    //6-2.
 //    @RequiresApi(api = Build.VERSION_CODES.N)
