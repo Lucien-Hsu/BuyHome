5648c65b68596b8013ec87114d2a4b7a4fb067b3
Lucien-Hsu <meteor831@gmail.com>
2020-12-20 17:34:29 +0800

    feat:可從相簿選擇圖片來替換大頭照
    #issue 063

--- app/src/main/java/com/example/buyhome_lcn/AccountInfoActivity.java
+++ app/src/main/java/com/example/buyhome_lcn/AccountInfoActivity.java
@@ -1,21 +1,38 @@
 package com.example.buyhome_lcn;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.example.buyhome_lcn.R;
 
+import java.io.FileNotFoundException;
+import java.lang.annotation.Target;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AccountInfoActivity extends AppCompatActivity {
+    private static final int GALLERY_REQUEST = 001;
+    private static final int CAMERA_REQUEST = 002;
+
     Context context;
+    CardView cvUserPhoto;
+    ImageView imgUserPhoto;
     ListView lvAccountArea;
 
     List<Map<String, Object>> itemList;
@@ -36,19 +53,28 @@ public class AccountInfoActivity extends AppCompatActivity {
 
     Integer[] showNextSign = {
             null, R.drawable.arrow_right,
-            R.drawable.arrow_right,  R.drawable.arrow_right,
-            R.drawable.arrow_right,  R.drawable.arrow_right};
+            R.drawable.arrow_right, R.drawable.arrow_right,
+            R.drawable.arrow_right, R.drawable.arrow_right};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_info);
         context = this;
+        imgUserPhoto = findViewById(R.id.img_user_photo);
+
+        cvUserPhoto = findViewById(R.id.cv_user_photo);
+        cvUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getImageFromGallery();
+            }
+        });
 
         //TODO 製作listView清單
         itemList = new ArrayList<Map<String, Object>>();
 
-        for(int i = 0 ; i < infoImgList.length ; i++){
+        for (int i = 0; i < infoImgList.length; i++) {
             Map<String, Object> item = new HashMap<String, Object>();
             item.put("img", infoImgList[i]);
             item.put("info", infoTextList[i]);
@@ -66,4 +92,34 @@ public class AccountInfoActivity extends AppCompatActivity {
 
         lvAccountArea.setAdapter(adapter);
     }
+
+    private void getImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //1-3.若 requestCode 是GALLERY_REQUEST & 正常讀取 & 有資料則做
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && data != null){
+
+            //以 Uri 型態取得資料
+            Uri imageUri = data.getData();
+            //將圖片設定到指定 view
+            imgUserPhoto.setImageURI(imageUri);
+
+        }else if(requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
+            //2-3.若 requestCode 是 CAMERA_REQUEST & 正常讀取則做
+
+            //以 bitmap 型態變數儲存照片資料
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imgUserPhoto.setImageBitmap(bitmap);
+
+        }
+    }
+
 }
