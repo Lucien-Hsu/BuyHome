2d3743a8a36e195846a2befb3c6fb63636260f64
Lucien-Hsu <meteor831@gmail.com>
2021-01-25 19:43:07 +0800

    feat:save photo from camera/album to internal memory
    #issue 097

--- app/src/main/java/com/example/buyhome_lcn/fragment_member_area/AccountInfoFragment.java
+++ app/src/main/java/com/example/buyhome_lcn/fragment_member_area/AccountInfoFragment.java
@@ -1,6 +1,7 @@
 package com.example.buyhome_lcn.fragment_member_area;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,6 +17,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,6 +31,8 @@ import com.example.buyhome_lcn.R;
 import com.example.buyhome_lcn.data.MemberAreaViewModel;
 import com.example.buyhome_lcn.databinding.FragmentAccountInfoBinding;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -203,6 +207,9 @@ public class AccountInfoFragment extends Fragment {
                 viewModel.setUserPhotoBitmap(bitmap);
 //                viewModel.setHasPhoto(true);
 
+                //儲存圖片至內部記憶體
+                saveToInternalStorage(bitmap);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -214,6 +221,9 @@ public class AccountInfoFragment extends Fragment {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             viewModel.setUserPhotoBitmap(bitmap);
 //            viewModel.setHasPhoto(true);
+
+            //儲存圖片至內部記憶體
+            saveToInternalStorage(bitmap);
 
             binding.imgUserPhoto.setImageBitmap(bitmap);
         }
@@ -228,5 +238,35 @@ public class AccountInfoFragment extends Fragment {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 儲存圖片到內部記憶體
+     */
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(context.getApplicationContext());
+        // path to /data/data/com.example.buyhome_lcn/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.d("myTest", "儲存圖片成功，路徑:" + directory.getAbsolutePath());
+
+        return directory.getAbsolutePath();
     }
 }
